b1e82cd257cf01d3452d56e1ee3e93d72ae07358
Miriam Carballo Rodríguez <miriamcarballorodriguez@gmail.com>
2025-11-05 20:50:21 +0100

    Solved Exercise 29

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,12 +1,27 @@
 package com.campusdual.classroom;
 
+class DivisionByZeroException extends Exception {
+    public DivisionByZeroException(String message) {
+        super(message);
+
+    }
+}
+
 public class Exercise {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
-
+    public static int divisionWithCustomException(int dividend, int divisor) throws DivisionByZeroException{
+        if (divisor == 0) {
+            throw new DivisionByZeroException ("No se puede dividir por cero.");
+        }
+        return dividend / divisor;
     }
 
-    public static void main(String[] args) {
-
-    }
+        public static void main(String[] args) {
+            try {
+                int resultado = divisionWithCustomException(10, 0);
+                System.out.println("Resultado: " + resultado);
+            } catch (DivisionByZeroException e) {
+                System.out.println("Excepción capturada: " + e.getMessage());
+            }
+        }
 }
